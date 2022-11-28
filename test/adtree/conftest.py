dc4c772fcc480a633aad8c9fec0e095e5429bb4a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-11-28 15:08:40 +0000

    Reuse the approvals assertion

---
+++ test/adtree/conftest.py
@@ -0,0 +1,27 @@
+# content of conftest.py
+import filecmp
+import os
+
+import pytest
+
+from adtree.models import Node
+from adtree.renderer import Renderer
+from adtree.themes import Theme
+
+
+class ApprovalAssertion:
+
+    # noinspection PyMethodMayBeStatic
+    def verify(self, root_node: Node, test_code, theme: Theme) -> None:
+        renderer = Renderer(theme=theme, output_format="png")
+        filename_without_extension, ext = os.path.splitext(test_code.co_filename)
+        output_file_base = "{0}.{1}".format(filename_without_extension, test_code.co_name)
+        actual_output_file = "{0}.actual.dot".format(output_file_base)
+        expected_output_file = "{0}.expected.dot".format(output_file_base)
+        renderer.render(root_node=root_node, filename=actual_output_file)
+        assert filecmp.cmp(expected_output_file, actual_output_file), "dot file differs from expected"
+
+
+@pytest.fixture(scope="session")
+def approvals():
+    return ApprovalAssertion()
--- test/adtree/test_acc.py
+++ test/adtree/test_acc.py
@@ -1,16 +1,13 @@
 from __future__ import annotations
 
-import filecmp
 import inspect
-import os
 
 from adtree.models import Attack, Defence, AndGate
-from adtree.renderer import Renderer
 from adtree.themes import RedBlueFillTheme, RedGreenOutlineTheme, NoFormatTheme
 
 
-def create_tree_will_all_types_of_nodes():
-    return Attack("the goal", [
+def test_render_fill_theme(approvals):
+    tree = Attack("the goal", [
         Attack("path1", [
             Defence("defend path1", [
                 Attack("path1 defence defeated")
@@ -24,21 +21,28 @@ def create_tree_will_all_types_of_nodes():
             ]),
         ]),
     ])
+    approvals.verify(tree, inspect.currentframe().f_code, RedBlueFillTheme())
 
 
-def test_render_fill_theme():
-    _render_and_compare(create_tree_will_all_types_of_nodes(),
-                        inspect.currentframe().f_code,
-                        RedBlueFillTheme())
+def test_render_outline_theme(approvals):
+    tree = Attack("the goal", [
+        Attack("path1", [
+            Defence("defend path1", [
+                Attack("path1 defence defeated")
+            ])
+        ]),
+        Attack("path2", [
+            Attack("path2.1"),
+            AndGate([
+                Attack("path3.1"),
+                Attack("path3.2"),
+            ]),
+        ]),
+    ])
+    approvals.verify(tree, inspect.currentframe().f_code, RedGreenOutlineTheme())
 
 
-def test_render_outline_theme():
-    _render_and_compare(create_tree_will_all_types_of_nodes(),
-                        inspect.currentframe().f_code,
-                        RedGreenOutlineTheme())
-
-
-def test_share_node():
+def test_share_node(approvals):
     shared_node = Attack("shared node", [Defence("node 1 defence")])
     tree = Attack("root", [
         Attack("path1", [
@@ -49,10 +53,10 @@ def test_share_node():
         ]),
     ])
 
-    _render_and_compare(tree, inspect.currentframe().f_code, NoFormatTheme())
+    approvals.verify(tree, inspect.currentframe().f_code, NoFormatTheme())
 
 
-def test_do_not_share_and_gate():
+def test_do_not_share_and_gate(approvals):
     tree = Attack("don't share AND", [
         AndGate([
             Attack("path1"),
@@ -64,17 +68,4 @@ def test_do_not_share_and_gate():
         ])
     ])
 
-    _render_and_compare(tree, inspect.currentframe().f_code, NoFormatTheme())
-
-
-
-# ~~~~~~~~~~~~ Test support files ~~~~~~~~~~
-
-def _render_and_compare(root_node, test_code, theme):
-    renderer = Renderer(theme=theme, output_format="png")
-    filename_without_extension, ext = os.path.splitext(test_code.co_filename)
-    output_file_base = "{0}.{1}".format(filename_without_extension, test_code.co_name)
-    actual_output_file = "{0}.actual.dot".format(output_file_base)
-    expected_output_file = "{0}.expected.dot".format(output_file_base)
-    renderer.render(root_node=root_node, filename=actual_output_file)
-    assert filecmp.cmp(expected_output_file, actual_output_file), "dot file differs from expected"
+    approvals.verify(tree, inspect.currentframe().f_code, NoFormatTheme())
