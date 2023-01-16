13785dab42227ee68307753c1d0a541c99c9ecb2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-01-16 14:59:23 +0000

    Implement the analyser

--- src/adtree/analysers.py
+++ src/adtree/analysers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 
-from adtree.models import ADTree
+from adtree.models import ADTree, Node, NodeType
 
 
 class Analyser(ABC):
@@ -20,5 +20,34 @@ class IsDefendedAnalyser(Analyser):
     METADATA_KEY = "IS_DEFENDED"
 
     def analyse_tree(self, tree: ADTree):
-        tree.add_metadata(IsDefendedAnalyser.METADATA_KEY, True)
+        self._process_node(tree)
 
+    def _process_node(self, node: Node):
+        # Process children first (depth first traversal)
+        for child_node in node.get_child_nodes():
+            self._process_node(child_node)
+
+        # Count total children and total defended children
+        total_children = len(node.get_child_nodes())
+        total_defended = len(list(filter(IsDefendedAnalyser._is_node_defended, node.get_child_nodes())))
+        # print(str(node) + " - " + str(total_defended) + "/" + str(total_children))
+
+        # Handle child nodes
+        is_defended = False
+        if total_children == 0:
+            if node.get_node_type() == NodeType.DEFENCE:
+                is_defended = True
+        else:
+            # Handle the AND gate as a special case
+            if node.get_node_type() == NodeType.AND_GATE:
+                if total_defended > 0:
+                    is_defended = True
+            else:
+                if total_defended == total_children:
+                    is_defended = True
+
+        node.add_metadata(IsDefendedAnalyser.METADATA_KEY, is_defended)
+
+    @staticmethod
+    def _is_node_defended(node: Node):
+        return node.get_metadata(IsDefendedAnalyser.METADATA_KEY)
---
+++ test/adtree/test_analysers.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import inspect
+
+from adtree.analysers import IsDefendedAnalyser
+from adtree.models import Attack, Defence, AndGate, ADTree, Node, NodeType
+from adtree.themes import NoFormatTheme
+
+
+def test_is_defended(approvals):
+    tree = ADTree("REFS.01", Attack("the goal", [
+        Attack("path1", [
+            Defence("defend path1", [
+                Attack("path1 defence defeated")
+            ])
+        ]),
+        Attack("path2", [
+            Attack("path2.1"),
+            AndGate([
+                Attack("path3.1"),
+                Attack("path3.2", [
+                    Defence("defended")
+                ]),
+            ]),
+        ]),
+    ]))
+
+    analyser = IsDefendedAnalyser()
+    analyser.analyse_tree(tree)
+    approvals.verify(tree=tree,
+                     theme=CustomIsDefendedTheme(),
+                     test_code=inspect.currentframe().f_code)
+
+
+# ~~~~~~~~~~~~~~~~~~ Test support code ~~~~~~~~~~~~~~~~~~~
+
+class CustomIsDefendedTheme(NoFormatTheme):
+    def get_node_attrs_for(self, node: Node):
+        metadata_attrs = {
+            "style": "filled"
+        }
+        if node.get_node_type() == NodeType.DEFENCE:
+            metadata_attrs |= {
+                "shape": "box",
+            }
+        if node.get_node_type() == NodeType.AND_GATE:
+            metadata_attrs |= {
+                "shape": "triangle",
+            }
+        if node.has_metadata(IsDefendedAnalyser.METADATA_KEY):
+            fillcolor = "#C8FFCB" if node.get_metadata(IsDefendedAnalyser.METADATA_KEY) else "#FFD3D6"
+            metadata_attrs |= {
+                "fillcolor": fillcolor,
+            }
+        return metadata_attrs
